9cec06a5d90e219788e0bda5d98c77198ad838d7
majoseek <matuslaw.mati@gmail.com>
2021-07-27 23:10:54 +0200

    Added login auth

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,9 +1,22 @@
 import express from "express";
 const router = express.Router();
+import db_connection from "../services/database.js";
 
 //verify user login and password
 router.post("/login", (req, res) => {
-    res.send("login");
+    new Promise((resolve, reject) => {
+        db_connection.query(
+            `SELECT * FROM users WHERE login=${req.body.login} AND password=${req.body.password}`,
+            (err, result, fields) => {
+                if (err) throw err;
+                resolve(result);
+            }
+        );
+    }).then((result) => {
+        if (result.length > 0) res.send("Logged in");
+        else res.send("Incorrect login or password");
+        //TODO: Add JWT Token to auth process
+    });
 });
 
 //register new user
