f511a36a4e5e37825199488f6b6d5cccc9a7f6ad
majoseek <matuslaw.mati@gmail.com>
2021-08-06 15:46:07 +0200

    Added poppers to FileHolder

--- client/src/components/FileHolder/FileHolder.js
+++ client/src/components/FileHolder/FileHolder.js
@@ -1,9 +1,16 @@
-import { Button, Divider, Grid } from "@material-ui/core";
+import { Box, Button, Divider, Grid, Paper, Popper } from "@material-ui/core";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
 import React from "react";
 import PhotoIcon from "@material-ui/icons/Photo";
+import { useState } from "react";
+import { Fade } from "@material-ui/core";
 import "./FileHolder.css";
 export default function FileHolder() {
+    const [anchorEl, setAnchorEl] = useState(null);
+    const handleClick = (event) => {
+        setAnchorEl(anchorEl ? null : event.currentTarget);
+    };
+    const open = Boolean(anchorEl);
     return (
         <React.Fragment>
             <Grid container direction="row" className="FileHolder">
@@ -17,9 +24,32 @@ export default function FileHolder() {
                     Modified
                 </Grid>
                 <Grid item xs={1}>
-                    <Button>
+                    <Button onClick={handleClick} aria-describedby={"popper"}>
                         <MoreVertIcon />
                     </Button>
+                    <Popper
+                        id={"popper"}
+                        open={open}
+                        anchorEl={anchorEl}
+                        transition
+                        placement={"top"}
+                    >
+                        {({ TransitionProps }) => (
+                            <Fade {...TransitionProps} timeout={300}>
+                                <Paper>
+                                    <Box
+                                        display="flex"
+                                        justifyContent="center"
+                                        alignItems="stretch"
+                                        flexDirection="column"
+                                    >
+                                        <Button>Download</Button>
+                                        <Button>Delete</Button>
+                                    </Box>
+                                </Paper>
+                            </Fade>
+                        )}
+                    </Popper>
                 </Grid>
             </Grid>
             <Grid container direction="row">
